871469eac5c7ce133cede19c8e166adfa67dd57b
Jeffrey Su <jeffreysu1355@gmail.com>
2017-08-05 17:45:33 -0700

    added scripts for events

---
+++ event scripts/copypasta.py
@@ -0,0 +1,15 @@
+def printStuff():
+    f= open('dictionary.py','w')
+    total = 20
+    for x in range(total):
+        f.write('''
+<div class="mySlides">
+    <div class="numbertext">'''+ str(x+1) +''' / '''+str(total)+'''</div>
+    <img src="../../img/volleyballPics/v'''+str(x+1)+'''.jpg" class="slideImage">
+</div>\n''')
+        
+    for x in range(total):
+        f.write('<span class="dot" onclick="currentSlide('+ str(x) +')"></span>\n')
+    f.close()
+        
+printStuff()
