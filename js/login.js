6effdece7f7ddedc761498416c679513226ca62e
Jeffrey Su <jeffreysu1355@gmail.com>
2017-08-25 14:18:25 -0700

    fixing login page

---
+++ js/login.js
@@ -0,0 +1,47 @@
+  $(document).ready(function(){
+    firebase.auth().onAuthStateChanged(function(user){
+      alert(firebase.auth().currentUser.displayName)
+      if(user){
+        $("#asdf").text(firebase.auth().currentUser.displayName);
+      }
+      
+    });
+    alert("here");
+  });
+
+
+
+
+//var user = firebase.auth().currentUser;
+//firebase.auth().onAuthStateChanged(function(user){
+//  if(user){
+//    $("#asdf").text(user)
+//  }else{
+//    $("#asdf").text(user)
+//  }
+//})
+
+
+//$(".logg").click(function(){
+//  $("about").text("meme machine")
+//}
+//var user = firebase.auth().currentUser;
+//    if(user){
+//        $("about").text(user);
+//        alert("asdfsdf");
+//}  
+
+//   if(jQuery){
+//      alert("checked")
+//    }
+//    
+//    function checkLogin{
+//      alert("checked")
+//      var user = firebase.auth().currentUser;
+//      if(user){
+//        $("ul li:last").text(user);
+//        alert("asdfsdf");
+//      }  
+//    }
+//    $(".logg").click(function(){
+//      
