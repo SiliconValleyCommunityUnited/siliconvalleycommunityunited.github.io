bd5673867b738b82a5e8f525c77665686d1bfbbf
Jeffrey Su <jeffreysu1355@gmail.com>
2017-08-30 11:21:09 -0700

    adding youth events

--- js/login.js
+++ js/login.js
@@ -1,4 +1,16 @@
   
+var config = {
+  apiKey: "AIzaSyAEvvk4JUpBH5dTcWStG3riLy3Q7e4cgX8",
+  authDomain: "test-2ab4f.firebaseapp.com",
+  databaseURL: "https://test-2ab4f.firebaseio.com",
+  projectId: "test-2ab4f",
+  storageBucket: "test-2ab4f.appspot.com",
+  messagingSenderId: "577570294935"
+};
+firebase.initializeApp(config);
+
+
+
 $(document).ready(function(){
   firebase.auth().onAuthStateChanged(function(user){
     if(user){
@@ -6,6 +18,9 @@ $(document).ready(function(){
     }
   });
 });
+
+
+
 
 //firebase.auth().onAuthStateChanged(function(user){
 //    if(user){
