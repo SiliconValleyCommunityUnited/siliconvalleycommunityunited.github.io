93f32ee692e56aa6ff24ba9dcaaf2255098809f3
fishgoatman <wang.patrick57@gmail.com>
2018-01-15 14:18:11 -0800

    i

--- event scripts/rename.py
+++ event scripts/rename.py
@@ -1,11 +1,14 @@
 import os
+
 def rename():
     x = 0
-    
     path =  os.getcwd()
+    print(str(path))
     filenames = os.listdir(path)
 
     for filename in filenames:
-        x++
+        x += 1
         n1 = str('n'+str(x))
         os.rename(filename, n1)
+
+rename()
